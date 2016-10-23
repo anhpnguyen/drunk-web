8916a2147d4f1827bb2e32359de3dd889ce92c57
anhpnguyen.an@gmail.com <anhpnguyen.an@gmail.com>
2016-10-22 21:40:28 -0400

    added postgresql, db migration using alembic

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config.from_object(os.environment['APP_SETTINGS'])
+# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+from models import Result
+
+
 @app.route('/')
 def hello():
 	return "Hello World!"
+
 @app.route('/<name>')
 def hello_name(name):
 	return "Hello {}!".format(name)
+  
 if __name__ == '__main__':
 	app.run()
